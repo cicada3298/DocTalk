2c14f34b61be5e2aac439fef00325aa6282b4897
Vaibhav_Chaudhary <123466277+cicada3298@users.noreply.github.com>
2025-08-31 20:55:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -47,6 +47,7 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:3000",            // local frontend
   "https://doc-talk-five.vercel.app"  // deployed frontend
+  "https://doctalk-31u3.onrender.com"
 ];
 
 app.use(cors({
