a2462ab8e3fe88b4480f73cfcdfb6f2f0828dc80
Vaibhav_Chaudhary <123466277+cicada3298@users.noreply.github.com>
2025-09-15 13:54:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const swaggerDocs = require("./swagger/swagger");
 const { initializeRedis } = require("./redis/redisClient");
-const { graphqlHTTP } = require("express-graphql");
-const { makeExecutableSchema } = require("@graphql-tools/schema");
-const typeDefs = require("./graphql/schema");
-const resolvers = require("./graphql/resolvers");
 
 const {
   registerUser,
@@ -61,78 +56,9 @@ app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-// GraphQL endpoint
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  }),
-);
-
 // Initialize Redis client
 initializeRedis();
 
-/**
- * Serve swagger.json
- * This route returns the JSON definition for the API documentation.
- */
-app.get("/swagger.json", (req, res) => {
-  res.json(swaggerDocs);
-});
-
-/**
- * Serve Swagger UI from a CDN
- * This route returns HTML that loads the Swagger UI assets from a CDN
- * and points it to /swagger.json.
- */
-app.get("/api-docs", (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8" />
-        <title>DocuThinker API Docs</title>
-        <!-- Include the swagger-ui CSS from a CDN -->
-        <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui.css" />
-        <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist@4.15.5/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist@4.15.5/favicon-16x16.png" sizes="16x16" />
-        <style>
-          body {
-            margin: 0;
-            padding: 0;
-          }
-        </style>
-      </head>
-      <body>
-        <div id="swagger-ui"></div>
-        <!-- Include the Swagger UI bundle and standalone preset from a CDN -->
-        <script src="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui-bundle.js"></script>
-        <script src="https://unpkg.com/swagger-ui-dist@4.15.5/swagger-ui-standalone-preset.js"></script>
-        <script>
-          window.onload = function() {
-            // Build a system
-            const ui = SwaggerUIBundle({
-              url: '/swagger.json',
-              dom_id: '#swagger-ui',
-              presets: [
-                SwaggerUIBundle.presets.apis,
-                SwaggerUIStandalonePreset
-              ],
-              layout: "StandaloneLayout"
-            })
-            window.ui = ui
-          }
-        </script>
-      </body>
-    </html>
-  `);
-});
-
-// Redirect root route to /api-docs
-app.get("/", (req, res) => {
-  res.redirect("/api-docs");
-});
 
 // Logging Middleware
 app.use((req, res, next) => {
